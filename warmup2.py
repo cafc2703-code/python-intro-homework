c4af21e21654fe5b0bece126d9ff8b8609414b4c
carlos-costa-python <cafc2703@gmail.com>
2026-07-14 18:22:16 -0400

    Update warmup2.py

--- warmup2.py
+++ warmup2.py
@@ -1,2 +1,7 @@
-today = input("What is today's date?")
+# Navigation commands:
+# cd ~/Documents/python-intro-homework
+# cd week-2/assignment-2
+
+today = input("What is today's date? ")
+
 print(f"You said today is {today}.")
