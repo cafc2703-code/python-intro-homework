4f550746e5fb9269828829855f72cd9eee83c46c
carlos-costa-python <cafc2703@gmail.com>
2026-07-14 18:02:06 -0400

    Create warmup4.py
    Define the variable 'age' before printing it.

---
+++ warmup4.py
@@ -0,0 +1,14 @@
+"""
+Error message:
+NameError: name 'age' is not defined
+
+Cause:
+I tried to print the variable 'age' before creating it.
+
+Fix:
+I created the variable before printing it.
+"""
+
+age = 25
+
+print(f"My age is {age}.")
