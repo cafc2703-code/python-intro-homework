bd725ee8f2e7bbf53b041fab493ebfb7b70b6427
carlos-costa-python <cafc2703@gmail.com>
2026-07-14 17:59:43 -0400

    Create warmup3.py

---
+++ warmup3.py
@@ -0,0 +1,7 @@
+# git log --oneline output:
+# e1e4f6f Add week 2 assignment files
+# 1c3e108 Create warmup2.py
+# 015f2c5 Create warmup1.py
+# 91fffbe Create assignment-2
+
+print("This week i learned about the terminal, git and github.")
