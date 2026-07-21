5a4b809e2ee1217952027ac95877e667d317f889
carlos-costa-python <cafc2703@gmail.com>
2026-07-21 18:38:23 -0400

    Refactor print statements to use f-strings

--- warmup4.py
+++ warmup4.py
@@ -1,16 +1,17 @@
 # Warmup 4: Sign and Parity
 
 number = int(input("Enter a number: "))
-# Check the sign
-if number > 0:
-print(number, "is positive.")
-elif number < 0:
-print(number, "is negative.")
-else:
-print(number, "is zero.")
 
-# Check if the number is even or odd
-if number % 2 == 0:
-print(number, "is even.")
+# Sign
+if number > 0:
+    print(f"{number} is positive.")
+elif number < 0:
+    print(f"{number} is negative.")
 else:
-print(number, "is odd.")
+    print(f"{number} is zero.")
+
+# Parity
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
