6b4f871ee47c4b616df4950d99e44fb1c82e01b0
carlos-costa-python <cafc2703@gmail.com>
2026-07-21 18:37:12 -0400

    Update warmup1.py

--- warmup1.py
+++ warmup1.py
@@ -1,15 +1,16 @@
 # Warmup 1: Letter Grades
 
 score = 84
+
 if score >= 90:
-grade = "A"
+    print("Grade: A")
 elif score >= 80:
-grade = "B"
+    print("Grade: B")
 elif score >= 70:
-grade = "C"
+    print("Grade: C")
 elif score >= 60:
-grade = "D"
+    print("Grade: D")
 else:
-grade = "F"
+    print("Grade: F")
+
 print("Score:", score)
-print("Grade:", grade)
