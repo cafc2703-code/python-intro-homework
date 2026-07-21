f5825fff054fd92aceb50e448fe3cfdb8756c909
carlos-costa-python <cafc2703@gmail.com>
2026-07-21 18:06:31 -0400

    Update warmup2.py
    Added age categorization based on user input.

--- warmup2.py
+++ warmup2.py
@@ -1,2 +1,13 @@
-today = input("What is today's date?")
-print(f"You said today is {today}.")
+# Warmup 2: Age Categories
+
+age = int(input("Enter your age: "))
+if age >= 0 and age <= 12:
+print("You are a Child.")
+elif age >= 13 and age <= 17:
+print("You are a Teen.")
+elif age >= 18 and age <= 64:
+print("You are an Adult.")
+elif age >= 65:
+print("You are a Senior.")
+else:
+print("Invalid age.")
